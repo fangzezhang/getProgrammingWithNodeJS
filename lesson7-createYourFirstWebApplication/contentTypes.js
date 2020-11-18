82ea493e88d1bb610cf494bbbeeb68d8e99e2237
fang.zezhang <fang.zezhang@navercorp.com>
2020-11-18 13:47:56 +0800

    准备工作：配置content-type + util getFile 函数 + router

---
+++ lesson7-createYourFirstWebApplication/contentTypes.js
@@ -0,0 +1,20 @@
+module.exports = {
+  html: {
+    "Content-type": "text/html"
+  },
+  text: {
+    "Content-type": "text/plain"
+  },
+  js: {
+    "Content-type": "text/js"
+  },
+  jpg: {
+    "Content-type": "text/jpg"
+  },
+  png: {
+    "Content-type": "text/png"
+  },
+  css: {
+    "Content-type": "text/css"
+  },
+};
