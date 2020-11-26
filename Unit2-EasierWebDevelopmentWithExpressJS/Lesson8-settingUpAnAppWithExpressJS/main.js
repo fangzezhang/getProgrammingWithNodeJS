40bccd6adec83970db7bfd694cfb3c5a8eaca706
fang.zezhang <fang.zezhang@navercorp.com>
2020-11-26 14:39:37 +0800

    Lesson8 over

--- Unit2-EasierWebDevelopmentWithExpressJS/Lesson8-settingUpAnAppWithExpressJS/main.js
+++ Unit2-EasierWebDevelopmentWithExpressJS/Lesson8-settingUpAnAppWithExpressJS/main.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello express!')
+  console.info(req.params);
+  console.info(req.body);
+  console.info(req.url);
+  console.info(req.query);
+  res.send('hello express!');
 })
   .listen(port, () => {
-    console.info(`express server listening at ${port} port`)
+    console.info(`express server listening at localhost:${port}`)
   });
